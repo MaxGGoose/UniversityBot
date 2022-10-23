bd34bfe0fceca0c03d232ac78d90cbbee7c5475c
MaxGGoose <max.pink.goose@gmail.com>
2022-10-23 13:35:01 +0300

    Create bot client with StartBot and StopBot methods

---
+++ UniversityBot/BotClient.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using UniversityBot.TelegramHandlers;
+
+namespace UniversityBot;
+
+public class BotClient
+{
+    private readonly TelegramBotClient _tgBotClient;
+    private readonly CancellationTokenSource _cancellationTokenSource;
+
+    public required string TgBotToken { private get; init; }
+    
+    public BotClient()
+    {
+        _tgBotClient = new TelegramBotClient(TgBotToken!);
+        _cancellationTokenSource = new CancellationTokenSource();
+    }
+    
+    public async Task<User> StartBot()
+    {
+        _tgBotClient.StartReceiving<UpdateHandler>(
+            cancellationToken: _cancellationTokenSource.Token);
+
+        return await _tgBotClient.GetMeAsync();
+    }
+
+    public void StopBot() => _cancellationTokenSource.Cancel();
+}
