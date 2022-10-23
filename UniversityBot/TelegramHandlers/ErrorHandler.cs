2b06dfd4f9f01a91013271cab3828603387a4288
MaxGGoose <max.pink.goose@gmail.com>
2022-10-23 12:53:32 +0300

    Add handler for errors

---
+++ UniversityBot/TelegramHandlers/ErrorHandler.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+
+namespace UniversityBot.TelegramHandlers;
+
+public sealed class ErrorHandler
+{
+    public Task HandleErrorAsync(ITelegramBotClient tgBotClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]" +
+                   $"\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(errorMessage);
+        
+        return Task.CompletedTask;
+    }
+}
