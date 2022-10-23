695a019327ebef276f165177eb219830f14933d6
MaxGGoose <max.pink.goose@gmail.com>
2022-10-23 12:53:18 +0300

    Add handler for incoming messages

---
+++ UniversityBot/TelegramHandlers/UpdateHandler.cs
@@ -0,0 +1,23 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace UniversityBot.TelegramHandlers;
+
+public sealed class UpdateHandler
+{
+    public Task HandleUpdateAsync(ITelegramBotClient tgBotClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Type != UpdateType.Message) return Task.CompletedTask;
+        if (update.Message!.Type != MessageType.Text) return Task.CompletedTask;
+        
+        var message = update.Message;
+
+        Console.WriteLine($"Received a '{message.Text}' in chat with @{message.From!.Username}");
+        
+        //TODO: Create commands itself
+
+        return Task.CompletedTask;
+
+    }
+}
