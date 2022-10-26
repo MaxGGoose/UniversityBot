fbc6d38c0713f566fe09c032623ce308e86deb3b
MaxGGoose <max.pink.goose@gmail.com>
2022-10-26 19:41:14 +0300

    Create file downloader

---
+++ UniversityBot/FileDownloader.cs
@@ -0,0 +1,16 @@
+﻿using File = System.IO.File;
+
+namespace UniversityBot;
+
+public static class FileDownloader
+{
+    public static async Task Download(string linkToFile)
+    {
+        var httpResult = await HttpClientInstance.GetInstance().GetAsync(linkToFile);
+
+        await using var resultStream = await httpResult.Content.ReadAsStreamAsync();
+        await using var fileStream = File.Create(Directory.GetCurrentDirectory() + $@"\{linkToFile.Split("/").Last()}");
+
+        await resultStream.CopyToAsync(fileStream);
+    }
+}
