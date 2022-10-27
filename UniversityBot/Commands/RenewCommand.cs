fdc6dfebe036007f04f642b024f8b78a6790ef6f
MaxGGoose <max.pink.goose@gmail.com>
2022-10-27 22:04:40 +0300

    Add RenewCommand.cs just for my uses

---
+++ UniversityBot/Commands/RenewCommand.cs
@@ -0,0 +1,36 @@
+﻿using PuppeteerSharp;
+using ScheduleFilesDownloader;
+
+namespace UniversityBot.Commands;
+
+public class RenewCommand : ICommand
+{
+    public Task<Action> Command(ITelegramBotClient tgBotClient, Message message, CancellationToken cancellationToken,
+        IsAnswerRequested isAnswerRequsted)
+    {
+        async void Action()
+        {
+            string answer;
+            if (message.Chat.Username == "max_gus")
+            {
+                var success = await ScheduleFilesRenewer.Renew();
+            
+                answer = success 
+                    ? "File successfully renewed"
+                    : "File doesn't need a renew";
+            }
+            else
+            {
+                answer = "Недостаточно прав для использования этой команды";
+            }
+            
+            
+            await tgBotClient.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: answer,
+                cancellationToken: cancellationToken);
+        }
+
+        return Task.FromResult(Action);
+    }
+}
