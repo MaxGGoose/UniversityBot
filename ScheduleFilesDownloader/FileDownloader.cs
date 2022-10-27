d610d8471e68fbc3f11586d661cfe8d3dd62dfe5
MaxGGoose <max.pink.goose@gmail.com>
2022-10-27 18:43:44 +0300

    Add check for "schedule_files" directory existing

--- ScheduleFilesDownloader/FileDownloader.cs
+++ ScheduleFilesDownloader/FileDownloader.cs
@@ -9,6 +9,10 @@ public static class FileDownloader
         var httpResult = await HttpClientInstance.GetInstance().GetAsync(linkToFile);
 
         await using var resultStream = await httpResult.Content.ReadAsStreamAsync();
+        
+        if (!Directory.Exists(Directory.GetCurrentDirectory() + @"\schedule_files"))
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\schedule_files");
+        
         await using var fileStream = File.Create(Directory.GetCurrentDirectory() + $@"\{linkToFile.Split("/").Last()}");
 
         await resultStream.CopyToAsync(fileStream);
