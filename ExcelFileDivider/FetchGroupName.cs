f99fe473ffb7a0c1e10d261a37c728d2080da03c
MaxGGoose <max.pink.goose@gmail.com>
2022-10-30 00:31:29 +0300

    Add fetcher of group name from xlsx file

---
+++ ExcelFileDivider/FetchGroupName.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using OfficeOpenXml;
+
+namespace ExcelFileDivider;
+
+public static class FetchGroupName
+{
+    public static string Fetch(ExcelWorksheet excelWorksheet)
+    {
+        var regex = new Regex("[А-Яа-я]+[-]+[0-9]+[-]?[0-9]*[а-я]*");
+        var cells = new List<string>(1);
+        
+        for (var row = 10; row < 16; row++)
+        {
+            for (var col = 1; col < 5; col++)
+            {
+                cells.Add(excelWorksheet.Cells[row, col].Text);
+            }
+        }
+
+        var temp = string.Join(' ', cells).Replace(" - ", "-");
+
+        Console.WriteLine(regex.Match(temp).Value);
+        
+        return regex.Match(temp).Value;
+    }
+}
